86e5fee5a5669df4e79f157ee5c9f20f5a506a64
dban23 <david.ban@comping.hr>
2025-09-11 19:22:47 +0200

    Getting geolocation data from address

--- main.py
+++ main.py
@@ -2,15 +2,54 @@ from dotenv import load_dotenv
 import requests
 import json
 import os
+from geopy.geocoders import Nominatim
+import math
+
+
+def addr_to_latlon():
+    addr = input("Your current address: ")
+
+    geolocator = Nominatim(user_agent="show_flight")
+    location = geolocator.geocode(addr)
+
+    lat = location.latitude
+    lon = location.longitude
+
+    #print(f"Latitude: {lat}\nLongitude: {lon}")
+    return lat, lon
+
+
+def define_square():
+    lat, lon = addr_to_latlon()
+
+    # I need to make a square of 2km x 2km around the current location
+    # 1 km from some point transformed to longitute depends on the latitude
+    latrad = math.radians(lat)
+    lonkm = (111.32/2*math.cos(latrad))**-1
+
+    minlat = lat - 0.00898*2
+    maxlat = lat + 0.00898*2
+    minlon = lon - lonkm
+    maxlon = lon + lonkm
+
+    return f"{minlat} {minlon} {maxlat} {maxlon}"
+
 
 def get_flight():
     load_dotenv()
     apiKey = os.getenv("FLIGHTAWARE_API_KEY")
     apiUrl = "https://aeroapi.flightaware.com/aeroapi/flights/search"
     
+    #minlat = 45.695
+    #maxlat = 45.72093
+    #minlon = 15.988
+    #maxlon = 16.03191
+    
+    latlong = define_square()
+
     auth_header = {'x-apikey':apiKey}
     payload = {
-       "query": '-latlong "45.695 15.988 45.72093 16.03191"'
+       "query": f'-latlong "{latlong}"'
        #"query": '-latlong "45.68 15.8 45.9 16.1"'
     }
 
