3a5fac16338cf0288f9b930ee2dc5bb5f7270446
dban23 <david.ban@comping.hr>
2025-10-08 23:48:42 +0200

    Adding full functionality to the web app

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from main import Flight_locator
 
 app = Flask(__name__)
 
 @app.route("/")
-def func():
+def make_page():
     return render_template("index.html")
+
+@app.route("/run_get_flight", methods=['POST'])
+def locate_flight():
+    address = request.form.get('address')
+    flight_locator = Flight_locator(address)
+
+    return flight_locator.get_flight()
+
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -5,74 +5,77 @@ import os
 from geopy.geocoders import Nominatim
 import math
 
+load_dotenv()
 
-def addr_to_latlon():
-    addr = input("Your current address: ")
+class Flight_locator:
+    def __init__(self, address):
+        self.address = address
 
-    geolocator = Nominatim(user_agent="show_flight")
-    location = geolocator.geocode(addr)
+    def addr_to_latlon(self):
+        #addr = input("Your current address: ")
 
-    lat = location.latitude
-    lon = location.longitude
+        geolocator = Nominatim(user_agent="show_flight")
+        location = geolocator.geocode(self.address)
 
-    #print(f"Latitude: {lat}\nLongitude: {lon}")
-    return lat, lon
+        lat = location.latitude
+        lon = location.longitude
 
-
-def define_square():
-    lat, lon = addr_to_latlon()
-
-    # I need to make a square of 2km x 2km around the current location
-    # 1 km from some point transformed to longitute depends on the latitude
-    latrad = math.radians(lat)
-    lonkm = (111.32/2*math.cos(latrad))**-1
-
-    minlat = lat - 0.00898*2
-    maxlat = lat + 0.00898*2
-    minlon = lon - lonkm
-    maxlon = lon + lonkm
-
-    return f"{minlat} {minlon} {maxlat} {maxlon}"
+        #print(f"Latitude: {lat}\nLongitude: {lon}")
+        return lat, lon
 
 
-def get_flight():
-    load_dotenv()
-    apiKey = os.getenv("FLIGHTAWARE_API_KEY")
-    apiUrl = "https://aeroapi.flightaware.com/aeroapi/flights/search"
-    
-    #minlat = 45.695
-    #maxlat = 45.72093
-    #minlon = 15.988
-    #maxlon = 16.03191
-    
-    latlong = define_square()
+    def define_square(self):
+        lat, lon = self.addr_to_latlon()
 
-    auth_header = {'x-apikey':apiKey}
-    payload = {
-       "query": f'-latlong "{latlong}"'
-       #"query": '-latlong "45.68 15.8 45.9 16.1"'
-    }
+        # I need to make a square of 2km x 2km around the current location
+        # 1 km from some point transformed to longitute depends on the latitude
+        latrad = math.radians(lat)
+        lonkm = (111.32/2*math.cos(latrad))**-1
 
-    response = requests.get(apiUrl, headers=auth_header, params=payload)
-    code = response.status_code
-    resp_json = response.json()
-    
-    flights = resp_json["flights"]
-    #flights = json.dumps(flights_bef, indent=4, ensure_ascii=False)
+        minlat = lat - 0.00898*2
+        maxlat = lat + 0.00898*2
+        minlon = lon - lonkm
+        maxlon = lon + lonkm
 
-    if code == 200:
-        if flights == []:
-            print("Can't detect any flights around.")
+        return f"{minlat} {minlon} {maxlat} {maxlon}"
+
+
+    def get_flight(self):
+        apiKey = os.getenv("FLIGHTAWARE_API_KEY")
+        apiUrl = "https://aeroapi.flightaware.com/aeroapi/flights/search"
+        
+        #minlat = 45.695
+        #maxlat = 45.72093
+        #minlon = 15.988
+        #maxlon = 16.03191
+        
+        latlong = self.define_square()
+
+        auth_header = {'x-apikey':apiKey}
+        payload = {
+        "query": f'-latlong "{latlong}"'
+        #"query": '-latlong "45.68 15.8 45.9 16.1"'
+        }
+
+        response = requests.get(apiUrl, headers=auth_header, params=payload)
+        code = response.status_code
+        resp_json = response.json()
+        
+        flights = resp_json["flights"]
+        #flights = json.dumps(flights_bef, indent=4, ensure_ascii=False)
+
+        if code == 200:
+            if flights == []:
+                return "Can't detect any flights around."
+            else:
+                output = ""
+                for flight in flights:
+                    flight_number = flight["ident"]
+                    from_name = flight["origin"]["name"]
+                    from_city = flight["origin"]["city"]
+                    to = flight["destination"]["name"]
+                    to_city = flight["destination"]["city"]
+                output += f"Flight number {flight_number} from {from_name}, {from_city} to {to}, {to_city}.<br>"
+                return output
         else:
-            for flight in flights:
-                flight_number = flight["ident"]
-                from_name = flight["origin"]["name"]
-                from_city = flight["origin"]["city"]
-                to = flight["destination"]["name"]
-                to_city = flight["destination"]["city"]
-                print(f"Flight number {flight_number} from {from_name}, {from_city} to {to}, {to_city}.")
-    else:
-        print(f"Error executing request: {code}")
-
-
-get_flight()
+            return f"Error executing request: {code}"
